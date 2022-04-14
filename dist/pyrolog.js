5566aff5ff9d56ca2aa440d3d3b663096bf14454
pyrologic <64922965+pyrologic@users.noreply.github.com>
2022-04-14 19:02:20 +0200

    dist directory excluded from git

--- dist/pyrolog.js
+++
@@ -1,9 +0,0 @@
-(function (factory) {
-	typeof define === 'function' && define.amd ? define(factory) :
-	factory();
-})((function () { 'use strict';
-
-	console.log('Hello TypeScript!');
-
-}));
-//# sourceMappingURL=pyrolog.js.map
