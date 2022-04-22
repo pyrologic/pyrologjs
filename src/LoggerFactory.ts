c7e69f45611bab345d400e30301e07420b0aa4b6
pyrologic <64922965+pyrologic@users.noreply.github.com>
2022-04-22 18:44:59 +0200

    get default level from configuration

--- src/LoggerFactory.ts
+++ src/LoggerFactory.ts
@@ -3,10 +3,12 @@ import { Level } from "./Level";
 import { Logger } from "./Logger";
 import { PyroLogger } from "./PyroLogger";
 
+const DEFAULT_CONFIG = '@default';
+
 export class LoggerFactory {
 
     private _defLevel: Level;
-    private _cfg: Map<string, Level>;
+    private _config: Map<string, Level>;
     private _loggers: Map<string, PyroLogger>;
 
     /**
@@ -14,7 +16,7 @@ export class LoggerFactory {
      */
     constructor() {
         this._defLevel = Level.INFO;
-        this._cfg = new Map();
+        this._config = new Map();
         this._loggers = new Map();
     }
 
@@ -53,21 +55,41 @@ export class LoggerFactory {
         return this._loggers.get(name);
     }
 
+    /**
+     * checks whether a configuration for a logger exists
+     * @param name logger name
+     * @returns true if there's a configuration for the specified logger; false otherwise
+     */
     hasConfig(name: string): Boolean {
-        return this._cfg.has(name);
+        return this._config.has(name);
     }
 
+    /**
+     * retrieves the configured level of a logger
+     * @param name logger name
+     * @returns the level for that logger; if no configuration exists for the specified logger then the default level is returned
+     */
     getLevel(name: string): Level {
-        return this._cfg.has(name) ? this._cfg.get(name) : this._defLevel;
+        return this._config.has(name) ? this._config.get(name) : this._defLevel;
     }
 
+    /**
+     * appplies a logger configuration
+     * @param config array of configuration items
+     */
     applyConfiguration(config: ConfigItem[]): void {
         for ( let ci of config) {
             const level = Level[ci.level];
             const name = ci.name;
-            this._cfg.set(name, level);
-            if ( this._loggers.has(name) ) {
-                this._loggers.get(name).setLevel(level);
+            if ( DEFAULT_CONFIG === name ) {
+                // new default level
+                this.defaultLevel = level;
+            } else {
+                // set level for a logger
+                this._config.set(name, level);
+                if ( this._loggers.has(name) ) {
+                    this._loggers.get(name).setLevel(level);
+                }
             }
         }
     }
