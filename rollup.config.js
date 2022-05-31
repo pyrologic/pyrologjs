99b4c1a598fc6013e2daf1bfc893bdb32f885cd0
pyrologic <64922965+pyrologic@users.noreply.github.com>
2022-05-31 14:42:05 +0200

    rollup configuration improved

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,25 @@ import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 import rollupTimeStamp from "./build/rollup-plugin-timestamp";
 
+let wc = 0;
+
+function ruBuild() {
+    return {
+        name: 'ru-init',
+        buildStart() {
+            wc = 0;
+        },
+        buildEnd() {
+            if ( wc > 0 ) {
+                console.log('\x1b[33m%s\x1b[0m', `Build ended with ${wc} warnings!`);
+            } else {
+                console.log('\x1b[32m%s\x1b[0m', 'Build ended with no warnings.');
+            }
+        }
+    }
+}
+
+
 const config = [
     {
         input: 'src/main.ts',
@@ -14,8 +33,19 @@ const config = [
         },
         plugins: [
             typescript({ tsconfig: './tsconfig.json' }),
+            ruBuild(),
             rollupTimeStamp()
-        ]
+        ],
+        onwarn ( { loc, frame, message } ) {
+            if ( loc ) {
+                console.warn(`#${++wc}: ${loc.file} (${loc.line}:${loc.column}) ${message}`);
+                if ( frame ) {
+                    console.warn(frame);
+                }
+            } else {
+                console.warn(`#${++wc}: ${message}`);
+            }
+        }
     },
     {
         // path to your declaration files root
--- rollup.config.prod.js
+++ rollup.config.prod.js
@@ -3,6 +3,24 @@ import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 import { terser } from "rollup-plugin-terser";
 
+let wc = 0;
+
+function ruBuild() {
+    return {
+        name: 'ru-init',
+        buildStart() {
+            wc = 0;
+        },
+        buildEnd() {
+            if ( wc > 0 ) {
+                console.log('\x1b[33m%s\x1b[0m', `Build ended with ${wc} warnings!`);
+            } else {
+                console.log('\x1b[32m%s\x1b[0m', 'Build ended with no warnings.');
+            }
+        }
+    }
+}
+
 const config = [
     {
         input: 'src/main.ts',
@@ -13,7 +31,20 @@ const config = [
             sourcemap: true,
             plugins: [ terser() ]
         },
-        plugins: [ typescript({ tsconfig: './tsconfig-prod.json' }) ]
+        plugins: [
+            typescript({ tsconfig: './tsconfig-prod.json' }),
+            ruBuild()
+        ],
+        onwarn ( { loc, frame, message } ) {
+            if ( loc ) {
+                console.warn(`#${++wc}: ${loc.file} (${loc.line}:${loc.column}) ${message}`);
+                if ( frame ) {
+                    console.warn(frame);
+                }
+            } else {
+                console.warn(`#${++wc}: ${message}`);
+            }
+        }
     },
     {
         // path to your declaration files root
